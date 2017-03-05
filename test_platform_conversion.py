73ddd389b083976d1d5566ec136f00e33f087938
Wintermute0110 <wintermute0110@gmail.com>
2017-03-05 23:00:51 +0900

    Test platforms on latest AEL.

--- test_platform_conversion.py
+++ test_platform_conversion.py
@@ -12,11 +12,11 @@ import sys, os
 from AEL.resources.scrap import *
 
 # --- main ----------------------------------------------------------------------------------------
-print('{0:<28} {1:<34} {2:<9} {3:<5}'.format('AEL platform', 'TheGamesDB', 'GameFAQs', 'MobyGames'))
+print('{0:<28}  {1:<35}  {2:<9}  {3:<5}'.format('AEL platform', 'TheGamesDB', 'GameFAQs', 'MobyGames'))
 print('{0}'.format('-' * 90))
 for AEL_plat_name in AEL_platform_list:
     TGDB_plat = AEL_platform_to_TheGamesDB(AEL_plat_name)
     GFAQ_plat = AEL_platform_to_GameFAQs(AEL_plat_name)
     MG_plat   = AEL_platform_to_MobyGames(AEL_plat_name)
 
-    print('{0:<28} {1:<34} {2:<9} {3:<5}'.format(AEL_plat_name, TGDB_plat, GFAQ_plat, MG_plat))
+    print('{0:<28}  {1:<35}  {2:<9}  {3:<5}'.format(AEL_plat_name, TGDB_plat, GFAQ_plat, MG_plat))
