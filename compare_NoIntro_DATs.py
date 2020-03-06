7bc9aa879a5a6ca3b2a6c54592c003639065a60f
Wintermute0110 <wintermute0110@gmail.com>
2020-03-06 16:09:21 +0900

    Future script.

---
+++ compare_NoIntro_DATs.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Compares all the NoIntro standard DATs with the PClone DATs, just to see if they
+# have the same ROMs.
+
+# Copyright (c) 2020 Wintermute0110 <wintermute0110@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+
+# --- Python standard library ---
+from __future__ import unicode_literals
+import copy
+
+# --- Import AEL modules ---
+from AEL.resources.utils import *
+from AEL.resources.rom_audit import *
+
+# --- Functions -----------------------------------------------------------------------------------
+
+# --- Main ----------------------------------------------------------------------------------------
